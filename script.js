da6859f3caab925b1029622f6daccb20edaeba83
harshal342 <harshalwagh342@gmail.com>
2024-04-28 10:39:04 +0530

    added local storage

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-
-const todoList = [];
-function add(){
+let todoList = [];
+const listFromLocalStorage = localStorage.getItem("todoList");
+if(listFromLocalStorage)
+{
+    todoList = JSON.parse(listFromLocalStorage);
+    renderToDoList();
+}
+function add()
+{
     const inputElement = document.getElementById('input-todo');
     todoList.push(inputElement.value);
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+    inputElement.value = "";
+
 
     renderToDoList();
 }
