9ef2006e659de3cf359e4ecbb297a5fb9220d2ba
harshal342 <harshalwagh342@gmail.com>
2024-04-28 10:23:26 +0530

    todo list

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const todoList = [];
+function add(){
+    const inputElement = document.getElementById('input-todo');
+    todoList.push(inputElement.value);
+
+    renderToDoList();
+}
+
+function renderToDoList()
+{
+    const listContainer = document.getElementById('list-container');
+    listContainer.innerHTML = "";
+
+    for(const todoItem of todoList)
+    {
+        listContainer.innerHTML += `<div class="todoItem">
+            <input type="checkbox">
+            ${todoItem}</div>` ;
+    }
+}
